633c74b7f37b284446df9f84a894e65ae6c06131
Eric Murphy <murphye@gmail.com>
2018-03-30 13:02:25 -0400

    Add Spring Boot Maven Plugin. Customize static locations

--- src/main/java/com/redhat/App.java
+++ src/main/java/com/redhat/App.java
@@ -2,10 +2,12 @@ package com.redhat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
+
         SpringApplication.run(App.class, args);
     }
 }
